cd414c982dca8af503e2fa6d8bb07166a698c100
MarouanSidali <marwandx57@gmail.com>
2023-08-07 10:14:24 +0000

    Designed the Notes Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
-import { BrowserRouter , Routes , Route  } from "react-router-dom"
-import Notes from "./pages/Notes"
-import CreateNote from "./pages/CreateNote"
-import EditNote from "./pages/EditNote"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Notes from "./pages/Notes";
+import CreateNote from "./pages/CreateNote";
+import EditNote from "./pages/EditNote";
 
 const App = () => {
   return (
-    <BrowserRouter> 
+    <main id="app">
+      <BrowserRouter>
         <Routes>
-            <Route path="/" element={<Notes />}/>
-            <Route path="/create-note" element={< CreateNote />} />
-            <Route path="/edite-note/:id" element={<EditNote />} />
-            
+          <Route path="/" element={<Notes />} />
+          <Route path="/create-note" element={<CreateNote />} />
+          <Route path="/edite-note/:id" element={<EditNote />} />
         </Routes>
-    </BrowserRouter>
-  )
-}
+      </BrowserRouter>
+    </main>
+  );
+};
 
-export default App
+export default App;
