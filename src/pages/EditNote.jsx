d60473e208e26b20cab3b77cee19630dc43785b8
MarouanSidali <marwandx57@gmail.com>
2023-08-08 13:36:56 +0000

    Designed the edit note page

--- src/pages/EditNote.jsx
+++ src/pages/EditNote.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import { Link } from "react-router-dom";
+import { IoIosArrowBack } from "react-icons/io";
+import { RiDeleteBin6Line } from "react-icons/ri"
 
 const EditNote = () => {
   return (
-    <div>EditNote</div>
-  )
-}
+    <section>
+      <header className="create-note__header">
+        <Link to="/" className="btn">
+          <IoIosArrowBack />
+        </Link>
+        <button className="btn lg prinmary">Save</button>
+        <button className="btn danger"><RiDeleteBin6Line /></button>
+      </header>
+      <form className="create-note__form">
+        <input type="text" placeholder="Title" autoFocus />
+        <textarea placeholder="Note details..." rows="20"></textarea>
+      </form>
+    </section>
+  );
+};
 
-export default EditNote
+export default EditNote;
