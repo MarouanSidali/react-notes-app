8a7abfba5f3e7344cef55626c386f18217a43fea
MarouanSidali <marwandx57@gmail.com>
2023-08-10 09:27:44 +0000

    Create note functionality

--- src/pages/CreateNote.jsx
+++ src/pages/CreateNote.jsx
@@ -1,18 +1,43 @@
 import { Link } from "react-router-dom";
 import { IoIosArrowBack } from "react-icons/io";
+import { useState } from "react";
+import { v4 as uuid} from "uuid"
 
 const CreateNote = () => {
+  const [title, setTitle] = useState("");
+  const [details, setDetails] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(title && details) {
+      const note = {id: uuid() , title , details }
+      console.log(note)
+    }
+  
+  }
+
   return (
     <section>
       <header className="create-note__header">
         <Link to="/" className="btn">
           <IoIosArrowBack />
         </Link>
-        <button className="btn lg prinmary">Save</button>
+        <button className="btn lg prinmary" onClick={handleSubmit}>Save</button>
       </header>
-      <form className="create-note__form">
-        <input type="text" placeholder="Title" autoFocus />
-        <textarea placeholder="Note details..." rows="20"></textarea>
+      <form className="create-note__form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          autoFocus
+        />
+        <textarea
+          placeholder="Note details..."
+          value={details}
+          onChange={(e) => setDetails(e.target.value)}
+          rows="28"
+        ></textarea>
       </form>
     </section>
   );
